34f5d48a02b83600c89006dd7b2bc7bd6d0d3af2
Richard Fussenegger <richard@fussenegger.info>
2013-03-18 09:43:04 +0100

    Optimized animation.

--- task-2/public_html/public/js/main.js
+++ task-2/public_html/public/js/main.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
   $('#btn-show-register,#btn-show-login').click(function (event) {
     if (event) event.preventDefault();
     $.when(
-      $('form.js-visible').stop(1, 1).animate({ marginLeft: -300, opacity: 'hide' }, 500, 'linear')
+      $('form.js-visible').stop(1, 1).animate({ marginTop: -300, opacity: 'hide' })
     ).done(function () {
       $('form').toggleClass('js-hidden js-visible');
-      $('form.js-visible').animate({ marginLeft: 0, opacity: 'show' }, 500, 'linear');
+      $('form.js-visible').animate({ marginTop: 0, opacity: 'show' });
     });
     return false;
   });
