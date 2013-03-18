e2caf059f1e7e071281f781a8c4987a22bc81cae
Richard Fussenegger <richard@fussenegger.info>
2013-03-18 09:38:24 +0100

    Included code for the register form.

--- task-2/public_html/public/js/main.js
+++ task-2/public_html/public/js/main.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function () {
+  $('#btn-show-register,#btn-show-login').click(function (event) {
+    if (event) event.preventDefault();
+    $.when(
+      $('form.js-visible').stop(1, 1).animate({ marginLeft: -300, opacity: 'hide' }, 500, 'linear')
+    ).done(function () {
+      $('form').toggleClass('js-hidden js-visible');
+      $('form.js-visible').animate({ marginLeft: 0, opacity: 'show' }, 500, 'linear');
+    });
+    return false;
+  });
+});
